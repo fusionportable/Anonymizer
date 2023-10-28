41538306a5c668d98fe03a6e0fabd5dd716a035a
yjw <jyubt@connect.ust.hk>
2023-10-29 00:42:47 +0800

    run on server13

--- run_batch.sh
+++ run_batch.sh
@@ -6,8 +6,8 @@ list=("handheld/room00/room00" "mini_hercules/campus00/campus00" "mini_hercules/
 for sequence_name in "${list[@]}"
 do
     echo "Processing $sequence_name"
-    input="/workspace/data/${sequence_name}.bag"
-    output="/workspace/data/${sequence_name}_anonymized.bag"
+    input="/workspace/anonymizer/data/${sequence_name}.bag"
+    output="/workspace/anonymizer/data/${sequence_name}_anonymized.bag"
     echo "Input: $input"
     echo "Output: $output"
     python anonymizer/bin/anonymize_bag.py --input $input --output $output --weights weights
